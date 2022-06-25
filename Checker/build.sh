4c974339114f8f26823ce572d2fc1d00b7dbb442
shsa <shilovsa@gmail.com>
2022-06-25 13:16:24 +0700

    .

--- Checker/build.sh
+++ Checker/build.sh
@@ -1,4 +1,5 @@
-docker build -t dotnet:checker ./images/dotnet/
+docker build -t dotnet:builder ./images/dotnet.builder/
+docker build -t dotnet:runtime ./images/dotnet.runtime/
 docker build -t python:checker ./images/python/
 docker build -t checker:latest ./images/checker/
 
